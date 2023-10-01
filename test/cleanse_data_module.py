beaa5b905b3a02c74cfe957c8f2dbe0b0ff9919f
Aekanun Thongtae <grizzlyimacssd@Aekanuns-iMac.local>
2023-10-01 10:08:22 +0700

    add testing

---
+++ test/cleanse_data_module.py
@@ -0,0 +1,57 @@
+import pytest
+from cleanse_data_module import cleanse_data
+
+def test_cleanse_data_string_fields():
+    data = {
+        "tr_time_str": " 2022-10-01 10:10:10 ",
+        "first_name": " John ",
+        "last_name": " Doe ",
+        "city": " New York ",
+        "state": " NY ",
+        "product": " Laptop "
+    }
+
+    cleansed_data = cleanse_data(data)
+
+    assert cleansed_data["tr_time_str"] == "2022-10-01 10:10:10"
+    assert cleansed_data["first_name"] == "John"
+    assert cleansed_data["last_name"] == "Doe"
+    assert cleansed_data["city"] == "New York"
+    assert cleansed_data["state"] == "NY"
+    assert cleansed_data["product"] == "Laptop"
+
+def test_cleanse_data_invalid_amount():
+    data = {
+        "amount": "invalid_amount"
+    }
+
+    cleansed_data = cleanse_data(data)
+
+    assert cleansed_data["amount"] == None
+
+def test_cleanse_data_valid_amount():
+    data = {
+        "amount": "100.5"
+    }
+
+    cleansed_data = cleanse_data(data)
+
+    assert cleansed_data["amount"] == 100.5
+
+def test_cleanse_data_valid_date():
+    data = {
+        "tr_time_str": "2022-10-01 10:10:10"
+    }
+
+    cleansed_data = cleanse_data(data)
+
+    assert cleansed_data["dayofweek"] == 5  # Saturday
+
+def test_cleanse_data_invalid_date():
+    data = {
+        "tr_time_str": "invalid_date"
+    }
+
+    cleansed_data = cleanse_data(data)
+
+    assert cleansed_data["dayofweek"] == None
